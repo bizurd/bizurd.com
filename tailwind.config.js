20601015b6011178194fa6c52ecc5cdae238d5df
Andre van Tonder <drevan@hey.com>
2021-10-07 17:52:46 +1000

    Start work on temporary landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,45 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.blueGray,
+      blue: colors.sky,
+      purple: colors.purple,
+    },
+    extend: {
+      animation: {
+       'swing': 'swing 1.5s ease-in-out infinite',
+      },
+      keyframes: {
+        swing: {
+          '20%': {
+            transform: 'rotate3d(0, 0, 1, 15deg)'
+          },
+        
+          '40%': {
+            transform: 'rotate3d(0, 0, 1, -10deg)'
+          },
+        
+          '60%': {
+            transform: 'rotate3d(0, 0, 1, 5deg)'
+          },
+        
+          '80%': {
+            transform: 'rotate3d(0, 0, 1, -5deg)'
+          },
+        
+          'to': {
+            transform: 'rotate3d(0, 0, 1, 0deg)'
+          },
+        }
+       }
+    }
+  },
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
 };
